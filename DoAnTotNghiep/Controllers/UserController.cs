76e188ae79aa07853de2713f44a2981b5ebcd519
lanhnv2697@gmail.com <lanhnv2697@gmail.com>
2020-06-24 21:21:48 +0700

    gi

---
+++ DoAnTotNghiep/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using DoAnTotNghiep.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebData.Implementation;
+using WebData.Models;
+
+namespace DoAnTotNghiep.Controllers
+{
+    public class UserController : Controller
+    {
+    
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IMapper _mapper;
+        private readonly IUserService _IUserServed;
+
+
+        public UserController(
+            IHostingEnvironment hostingEnvironment,
+            IMapper mapper,
+            IUserService usersv)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _mapper = mapper;
+            _IUserServed = usersv;
+
+        }
+
+        public IActionResult Index(int id)
+        {
+            var model = _IUserServed.GetById(id);
+            var userVM = _mapper.Map<UserVM>(model);            
+            return View(userVM);
+        }
+        public IActionResult Teacher(int id)
+        {
+           
+            var model = _IUserServed.GetById(id);
+            var userVM = _mapper.Map<UserVM>(model);
+            
+         return View(userVM);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UserVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //Create
+                        var model = _mapper.Map<Users>(vm);
+                        await _IUserServed.UpdateAsync(model);
+                        ViewBag.IsSuccess = true;
+                        return RedirectToAction(nameof(Index));
+                  
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.IsSuccess = false;
+                    return View(vm);
+                }
+            }
+            return View(vm);
+        }
+
+      
+      
+    }
+}
--- DoAnTotNghiep/Models/UserVM.cs
+++ DoAnTotNghiep/Models/UserVM.cs
@@ -11,13 +11,11 @@ namespace DoAnTotNghiep.Models
         public string FullName { get; set; }
         public bool? Sex { get; set; }
         public DateTime? Birthday { get; set; }
-        [Required(ErrorMessage ="Vui lòng nhập Email")]
-        [EmailAddress(ErrorMessage ="Email chưa đúng định dạng")]
         public string Email { get; set; }
-        [Required(ErrorMessage ="Vui lòng nhập mật khẩu")]
+      
         public string Password { get; set; }
         [NotMapped] // Does not effect with your database
-        [Compare("Password",ErrorMessage ="Mật khẩu chưa khớp")]
+   
         public string RePassword { get; set; }
         public string Token { get; set; }
         public string Introduction { get; set; }
