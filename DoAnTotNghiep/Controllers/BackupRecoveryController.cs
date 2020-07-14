c9b561d642f1a841b995124fffc661c103b12fcf
kn1171998 <2001160303@ittc.edu.vn>
2020-07-14 09:11:49 +0700

    backup and recovery

---
+++ DoAnTotNghiep/Controllers/BackupRecoveryController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using DoAnTotNghiep.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebData.Implementation;
+using WebData.Models;
+
+namespace DoAnTotNghiep.Controllers
+{
+    public class BackupRecoveryController : Controller
+    {
+        public BackupRecoveryController()
+        {
+        }
+        public IActionResult ConfigBackup()
+        {
+            return View();
+        }
+    }
+}
